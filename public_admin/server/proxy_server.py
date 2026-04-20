ed594b854c9a51f48dbeeec4b5a81717120afff1
Admin <admin@ak2025.vip>
2026-04-20 21:39:07 +0800

    fix: 补齐IM附件模块静态路由

--- public_admin/server/proxy_server.py
+++ public_admin/server/proxy_server.py
@@ -7715,6 +7715,9 @@ def _iter_widget_asset_paths() -> list[str]:
         os.path.join(PLUGINS_DIR, "im", "user", "im_entry.js"),
         os.path.join(PLUGINS_DIR, "im", "user", "im_client.js"),
         os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_app_shell.js"),
+        os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_image_manage.js"),
+        os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_file_manage.js"),
+        os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_plus_entry_manage.js"),
         os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_emoji_manage.js"),
         os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_voice_hold_manage.js"),
         os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_profile.js"),
@@ -7902,6 +7905,33 @@ async def im_user_plugin_client_js(request: Request):
 async def im_user_plugin_app_shell_module_js(request: Request):
 
     js_path = os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_app_shell.js")
+
+    return _build_widget_script_response(request, js_path)
+
+
+@app.get("/chat/plugins/im/user/modules/im_image_manage.js")
+
+async def im_user_plugin_image_manage_module_js(request: Request):
+
+    js_path = os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_image_manage.js")
+
+    return _build_widget_script_response(request, js_path)
+
+
+@app.get("/chat/plugins/im/user/modules/im_file_manage.js")
+
+async def im_user_plugin_file_manage_module_js(request: Request):
+
+    js_path = os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_file_manage.js")
+
+    return _build_widget_script_response(request, js_path)
+
+
+@app.get("/chat/plugins/im/user/modules/im_plus_entry_manage.js")
+
+async def im_user_plugin_plus_entry_manage_module_js(request: Request):
+
+    js_path = os.path.join(PLUGINS_DIR, "im", "user", "modules", "im_plus_entry_manage.js")
 
     return _build_widget_script_response(request, js_path)
 
